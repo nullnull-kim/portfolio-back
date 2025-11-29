6e8913628b6cbe7664c306db46ffebf9edb15ff5
nullnull-kim <nullnull.kim@gmail.com>
2025-11-29 21:13:01 +0900

    init

---
+++ src/test/kotlin/com/nullnull/portfolio/PortfolioBackApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.nullnull.portfolio
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class PortfolioBackApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
