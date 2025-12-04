e8bb5f50cb02b5648705cfe1435d33be0a8a80a8
nullnull-kim <nullnull.kim@gmail.com>
2025-12-05 00:34:47 +0900

    test: otherExperienceTest 테스트 기능 수정 및 추가

--- src/test/kotlin/com/nullnull/portfolio/service/OtherExperienceServiceTest.kt
+++ src/test/kotlin/com/nullnull/portfolio/service/OtherExperienceServiceTest.kt
@@ -1,7 +1,6 @@
 package com.nullnull.portfolio.service
 
 import org.assertj.core.api.Assertions.*
-import com.nullnull.portfolio.domain.OtherExperience
 import com.nullnull.portfolio.domain.Profile
 import com.nullnull.portfolio.repository.OtherExperienceRepository
 import com.nullnull.portfolio.repository.ProfileRepository
@@ -23,6 +22,7 @@ class OtherExperienceServiceTest @Autowired constructor(
 ){
 
     private lateinit var profile: Profile
+    private lateinit var command0: CreateOtherExperienceCommand
     private lateinit var command1: CreateOtherExperienceCommand
 
 
@@ -38,26 +38,69 @@ class OtherExperienceServiceTest @Autowired constructor(
         )
         profileRepository.save(profile)
 
-        command1 = CreateOtherExperienceCommand(
+        command0 = CreateOtherExperienceCommand(
             title = "정보통신산업진흥원 주관 다국적 해커톤",
             role = null,
             startedAt = LocalDate.of(2017, 6, 26),
             endedAt = LocalDate.of(2017, 6, 27),
             description = "외국학생과 한 팀을 이뤄 ‘홍익인간 기술’을 주제로 ICTㆍ친환경 그린테크ㆍ융합테크 등 4차 산업혁명 시대의 전반적인 영역에 관한 경연 및 시제품 제작"
         )
+
+        command1 = CreateOtherExperienceCommand(
+            title = "인프런, '재고시스템으로 알아보는 동시성이슈 해결방법' 수료",
+            role = null,
+            startedAt = LocalDate.of(2022, 8, 13),
+            endedAt = LocalDate.of(2022, 8, 24),
+            description = "재고시스템을 작접 만들면서 Application Level, Database Lock, Redis Distributed Lock을 통해 동시성 이슈를 해결하는 방법을 학습"
+        )
     }
 
     @Test
-    fun `otherExperience를 create할 때 profile의 isEnabled가 false이면 에러가 발생한다`() {
-
+    fun `create 정상 동작테스트`() {
         profile.disable()
         profileRepository.save(profile)
 
         // when & then
         assertThatThrownBy{
-            otherExperienceService.create(profile.id!!, command1)
+            otherExperienceService.create(profile.id!!, command0)
         }
             .isInstanceOf(IllegalArgumentException::class.java)
-//            .hasMessageContaining("profile is disabled")
+
+        profile.enable()
+        profileRepository.save(profile)
+
+        val enabledProfile = otherExperienceService.create(profile.id!!, command0)
+
+        val found = otherExperienceRepository.findById(enabledProfile.id!!).orElseThrow()
+
+        assertThat(found.title).isEqualTo(command0.title)
+        assertThat(found.description).isEqualTo(command0.description)
+        assertThat(found.startedAt).isEqualTo(command0.startedAt)
+        assertThat(found.endedAt).isEqualTo(command0.endedAt)
+        assertThat(found.role).isEqualTo(command0.role)
+    }
+
+    @Test
+    fun `getAllByProfile 테스트`(){
+        profile.disable()
+        profileRepository.save(profile)
+        assertThatThrownBy{
+            otherExperienceService.getAllByProfile(profile.id!!)
+        }
+            .isInstanceOf(IllegalArgumentException::class.java)
+
+        profile.enable()
+        profileRepository.save(profile)
+
+        otherExperienceService.create(profile.id!!, command0)
+        otherExperienceService.create(profile.id!!, command1)
+
+        val otherExperiences = otherExperienceService.getAllByProfile(profile.id!!)
+
+        assertThat(otherExperiences).hasSize(2)
+        assertThat(otherExperiences[0].title).isEqualTo(command0.title)
+        assertThat(otherExperiences[1].title).isEqualTo(command1.title)
+
+
     }
 }
