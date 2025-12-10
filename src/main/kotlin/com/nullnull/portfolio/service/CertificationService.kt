480686e1a1f3c1c46c3e017d5e21b8e1a92951d5
nullnull-kim <nullnull.kim@gmail.com>
2025-12-10 23:18:14 +0900

    feat: certificationService 작성

--- src/main/kotlin/com/nullnull/portfolio/service/CertificationService.kt
+++ src/main/kotlin/com/nullnull/portfolio/service/CertificationService.kt
@@ -1,20 +1,53 @@
 package com.nullnull.portfolio.service
 
+import com.nullnull.portfolio.domain.Certification
+import com.nullnull.portfolio.domain.Profile
 import com.nullnull.portfolio.repository.CertificationRepository
 import com.nullnull.portfolio.repository.ProfileRepository
-import org.springframework.context.annotation.Profile
 import org.springframework.stereotype.Service
+import java.time.LocalDate
 
-class CreateCertificationCommand {
-
-}
+data class CreateCertificationCommand(
+    val name: String,
+    val issuedBy: String,
+    val issuedAt: LocalDate
+)
 
 @Service
 class CertificationService(
     private val profileRepository: ProfileRepository,
     private val certificationRepository: CertificationRepository,
-){
-    fun create(profile: Profile, createCommand: CreateCertificationCommand) {
+) {
+    fun create(profileId: Long, createCommand: CreateCertificationCommand): Certification {
+        val profile = profileRepository.findById(profileId)
+            .orElseThrow {
+                IllegalArgumentException("Profile Not Found Id [$profileId]")
+            }
 
+        if(profile.enabled == false) {
+            throw IllegalArgumentException("Profile Disabled")
+        }
+
+        val certification = Certification(
+            profile = profile,
+            name = createCommand.name,
+            issuedBy = createCommand.issuedBy,
+            issuedAt = createCommand.issuedAt,
+        )
+
+        return certificationRepository.save(certification)
+    }
+
+    fun getAllByProfile(profileId: Long): List<Certification> {
+        val profile = profileRepository.findById(profileId)
+            .orElseThrow{
+                IllegalArgumentException("Profile Not Found Id [$profileId]")
+            }
+
+        if(profile.enabled == false) {
+            throw IllegalArgumentException("Profile Disabled")
+        }
+
+        return certificationRepository.findAllByProfileAndEnabledTrue(profile)
     }
 }
