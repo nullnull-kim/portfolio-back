df7bc712a3690a890a1b60938c903853ca2c93e6
nullnull-kim <nullnull.kim@gmail.com>
2025-12-13 16:04:41 +0900

    feat:Profile 도메인 수정

--- src/main/kotlin/com/nullnull/portfolio/domain/Profile.kt
+++ src/main/kotlin/com/nullnull/portfolio/domain/Profile.kt
@@ -3,6 +3,7 @@ package com.nullnull.portfolio.domain
 import com.nullnull.portfolio.domain.common.BaseEntity
 import jakarta.persistence.Column
 import jakarta.persistence.Entity
+import java.time.LocalDate
 
 @Entity
 class Profile (
@@ -12,14 +13,18 @@ class Profile (
     @Column(columnDefinition = "TEXT")
     val summary: String,
 
+    val name: String,
+
     val contactEmail: String,
 
     val github: String? = null,
 
     val blog: String? = null,
 
+    val birthday: LocalDate,
+
     val profileImageUrl: String? = null,
 
-) : BaseEntity()
+    ) : BaseEntity()
 {
 }
---
+++ src/test/kotlin/com/nullnull/portfolio/service/ProfileServiceTest.kt
@@ -0,0 +1,16 @@
+package com.nullnull.portfolio.service
+
+import org.junit.jupiter.api.Test
+
+import org.junit.jupiter.api.Assertions.*
+
+class ProfileServiceTest {
+
+    @Test
+    fun create() {
+    }
+
+    @Test
+    fun getByProfileId() {
+    }
+}
